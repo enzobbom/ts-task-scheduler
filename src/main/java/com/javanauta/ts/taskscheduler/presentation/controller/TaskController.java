9a6089ea9e814ea9878ca7b66107f87a5b1a760c
Enzo Bom <enzobom@gmail.com>
2026-06-26 14:55:39 +0100

    Clean up: remove end points not used anymore

--- src/main/java/com/javanauta/ts/taskscheduler/presentation/controller/TaskController.java
+++ src/main/java/com/javanauta/ts/taskscheduler/presentation/controller/TaskController.java
@@ -2,22 +2,18 @@ package com.javanauta.ts.taskscheduler.presentation.controller;
 
 import com.javanauta.ts.taskscheduler.application.service.TaskService;
 import com.javanauta.ts.taskscheduler.domain.model.Task;
-import com.javanauta.ts.taskscheduler.domain.model.enums.NotificationStatus;
 import com.javanauta.ts.taskscheduler.presentation.dto.in.CreateTaskRequestDTO;
 import com.javanauta.ts.taskscheduler.presentation.dto.in.UpdateTaskRequestDTO;
 import com.javanauta.ts.taskscheduler.presentation.mapper.TaskMapper;
 import com.javanauta.ts.taskscheduler.presentation.response.SuccessResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -43,25 +39,8 @@ public class TaskController {
         return ResponseEntity.status(httpCode).body(successResponse);
     }
 
-    // To be removed (and used internally only)
-    @GetMapping("/events")
-    public ResponseEntity<SuccessResponse> findTasksByPeriod(
-            @RequestParam @NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant initialDateTime,
-            @RequestParam @NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant finalDateTime) {
-
-        List<Task> tasks = taskService.findTasksByTimePeriod(initialDateTime, finalDateTime);
-
-        HttpStatus httpCode = HttpStatus.OK;
-        SuccessResponse successResponse = new SuccessResponse(
-                httpCode,
-                taskMapper.toTaskDTOList(tasks));
-
-        return ResponseEntity.status(httpCode).body(successResponse);
-    }
-
     @GetMapping
     public ResponseEntity<SuccessResponse> findTasksByUserEmail() {
-
         List<Task> tasks = taskService.findTasksByUserEmail();
 
         HttpStatus httpCode = HttpStatus.OK;
@@ -78,22 +57,6 @@ public class TaskController {
 
         taskService.deleteTask(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    // To be removed (will be done internally once proper asynch communication with Notifier ms is implemented)
-    @PatchMapping("/status")
-    public ResponseEntity<SuccessResponse> updateTaskStatus(
-            @RequestParam("status") @NotNull NotificationStatus notificationStatus,
-            @RequestParam("id") @NotBlank String id) {
-
-        Task task = taskService.updateTaskStatus(notificationStatus, id);
-
-        HttpStatus httpCode = HttpStatus.OK;
-        SuccessResponse successResponse = new SuccessResponse(
-                httpCode,
-                taskMapper.toTaskDTO(task));
-
-        return ResponseEntity.status(httpCode).body(successResponse);
     }
 
     @PatchMapping
