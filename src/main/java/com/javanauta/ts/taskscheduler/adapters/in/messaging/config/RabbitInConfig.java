bc43589937095b61dbb89f99b0e8be4d87fec1ad
Enzo Bom <enzobom@gmail.com>
2026-07-14 16:41:05 +0100

    Small clean up

--- src/main/java/com/javanauta/ts/taskscheduler/adapters/in/messaging/config/RabbitInConfig.java
+++ src/main/java/com/javanauta/ts/taskscheduler/adapters/in/messaging/config/RabbitInConfig.java
@@ -1,15 +1,8 @@
 package com.javanauta.ts.taskscheduler.adapters.in.messaging.config;
 
-import com.javanauta.ts.events.notification.messaging.Exchanges;
-import com.javanauta.ts.events.notification.messaging.Queues;
-import com.javanauta.ts.events.notification.messaging.RoutingKeys;
 import com.javanauta.ts.taskscheduler.adapters.in.messaging.NotificationEventRecoverer;
 import org.aopalliance.aop.Advice;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
