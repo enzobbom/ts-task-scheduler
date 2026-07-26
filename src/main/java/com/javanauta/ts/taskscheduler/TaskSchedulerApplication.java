af37e894c297e5470938bd5061aa612c85917841
Enzo Bom <enzobom@gmail.com>
2026-07-26 21:26:22 +0100

    Remove now unnecessary Feign dependency and implementations (call to User service to get credentials replaced by JWT authentication on Gateway)

--- src/main/java/com/javanauta/ts/taskscheduler/TaskSchedulerApplication.java
+++ src/main/java/com/javanauta/ts/taskscheduler/TaskSchedulerApplication.java
@@ -4,11 +4,9 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
-@EnableFeignClients
 @EnableScheduling
 @EnableRabbit
 public class TaskSchedulerApplication {
