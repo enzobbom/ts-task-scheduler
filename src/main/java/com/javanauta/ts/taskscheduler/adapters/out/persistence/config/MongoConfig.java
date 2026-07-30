d3e5035a6fa38bc399f586ad30c7e971af0103c2
Enzo Bom <enzobom@gmail.com>
2026-07-30 21:54:02 +0100

    Fix MongoConfig Bean dependency injection and add uuid-representation for saving the User UUID to the database

--- src/main/java/com/javanauta/ts/taskscheduler/adapters/out/persistence/config/MongoConfig.java
+++ src/main/java/com/javanauta/ts/taskscheduler/adapters/out/persistence/config/MongoConfig.java
@@ -2,12 +2,15 @@ package com.javanauta.ts.taskscheduler.adapters.out.persistence.config;
 
 import com.javanauta.ts.taskscheduler.adapters.out.persistence.converter.ZoneIdReadConverter;
 import com.javanauta.ts.taskscheduler.adapters.out.persistence.converter.ZoneIdWriteConverter;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.UuidRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
@@ -20,14 +23,29 @@ import java.util.List;
 public class MongoConfig {
 
     @Bean
-    public MongoClient mongoClient(@Value("${spring.data.mongodb.uri}") String uri) {
+    public MongoClient mongoClient(
+            @Value("${spring.data.mongodb.uri}") String uri,
+            @Value("${spring.data.mongodb.uuid-representation}") UuidRepresentation uuidRepresentation) {
+
         log.info("Using MongoDB URI: {}", uri);
-        return MongoClients.create(uri);
+
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .uuidRepresentation(uuidRepresentation)
+                .applyConnectionString(new ConnectionString(uri))
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
     }
 
     @Bean
-    public MongoDatabaseFactory mongoDatabaseFactory(@Value("${spring.data.mongodb.uri}") String uri) {
-        return new SimpleMongoClientDatabaseFactory(uri);
+    public MongoDatabaseFactory mongoDatabaseFactory(
+            MongoClient mongoClient,
+            @Value("${spring.data.mongodb.database}") String databaseName) {
+
+        return new SimpleMongoClientDatabaseFactory(
+                mongoClient,
+                databaseName
+        );
     }
 
     @Bean
