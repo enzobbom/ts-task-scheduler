7291e65563e30cd4d69d692b7ce7b3dda0c8e14a
Enzo Bom <enzobom@gmail.com>
2026-08-10 20:13:02 +0100

    Remove unnecessary UserDetailsSerivce auto config

--- src/main/java/com/javanauta/ts/taskscheduler/TaskApplication.java
+++ src/main/java/com/javanauta/ts/taskscheduler/TaskApplication.java
@@ -4,9 +4,12 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = MongoAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class})
 @EnableScheduling
 @EnableRabbit
 public class TaskApplication {
